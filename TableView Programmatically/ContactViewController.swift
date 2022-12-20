22ef6d7faba35251b8099e312170c343629fe1d7
joricky91 <jonathanricky91@gmail.com>
2022-12-20 19:09:19 +0700

    Initial Commit

---
+++ TableView Programmatically/ContactViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView Programmatically
+//
+//  Created by Jonathan Ricky Sandjaja on 20/12/22.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
